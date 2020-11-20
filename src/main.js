7658f30d77c186a61c00a1d39a348ba748cfe8ff
曹信坤 <1554836482@qq.com>
2020-11-20 15:41:35 +0800

    [初始化]

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
+/*
+ * @,@Author: ,: your name
+ * @,@Date: ,: 2020-11-20 15:31:21
+ * @,@LastEditTime: ,: 2020-11-20 15:37:53
+ * @,@LastEditors: ,: Please set LastEditors
+ * @,@Description: ,: In User Settings Edit
+ * @,@FilePath: ,: \vue-init\src\main.js
+ */
 import Vue from 'vue'
 import App from './App.vue'
-
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+Vue.use(Antd)
 Vue.config.productionTip = false
 
 new Vue({
